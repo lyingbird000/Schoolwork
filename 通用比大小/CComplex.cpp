1a12e4769e1556a13926cdf5256e05f6f27597c2
lyingbird000 <125780607+lyingbird000@users.noreply.github.com>
2026-03-04 16:48:27 +0800

    Add files via upload

---
+++ 通用比大小/CComplex.cpp
@@ -0,0 +1,133 @@
+﻿#include<iostream>
+#include"CComplex.h"
+#include<cmath>
+using namespace std;
+//以下为复数类成员函数的实现：
+CComplex::CComplex(double zR , double zI ) :mR(zR), mI(zI) {
+    if (mI == 0) {
+        cout <<"<=" << mR;  // 只有实部
+    }
+    else if (mR == 0) {     //只有虚部
+        if (mI == 1)
+            cout << "<=" << "i";
+        else if (mI == -1)
+            cout << "<=" << "-i";
+        else
+            cout << "<=" << mI << "i";
+    }
+    else {
+        cout << "<=" << mR;
+        if (mI > 0) {
+            if (mI == 1)
+                cout << "+i";
+            else
+                cout << "+" << mI << "i";
+        }
+        else {
+            if (mI == -1)
+                cout << "-i";
+            else
+                cout << mI << "i";
+        }
+    }
+    cout << endl;
+}
+CComplex::~CComplex() {
+    if (mI == 0) {
+        cout  << mR ;  // 只有实部
+    }
+    else if (mR == 0) {     //只有虚部
+        if (mI == 1)
+            cout << "i";
+        else if (mI == -1)
+            cout  << "-i";
+        else
+            cout  << mI << "i";
+    }
+    else {
+        cout  << mR;
+        if (mI > 0) {
+            if (mI == 1)
+                cout << "+i";
+            else
+                cout << "+" << mI << "i";
+        }
+        else {
+            if (mI == -1)
+                cout << "-i";
+            else
+                cout << mI << "i";
+        }
+    }
+    cout << "=>"<<endl;
+}
+
+void CComplex::SetValue() {
+	mR = 0;
+	mI = 0;
+}
+void CComplex::SetValue(double zR , double zI ) {
+	mR = zR;
+	mI = zI;
+}
+void CComplex::ShowValue() {
+    if (mI == 0) {
+        cout << "<=" << mR;  // 只有实部
+    }
+    else if (mR == 0) {      //只有虚部
+        if (mI == 1)
+            cout << "<=" << "i";
+        else if (mI == -1)
+            cout << "<=" << "-i";
+        else
+            cout << "<=" << mI << "i";
+    }
+    else {
+        cout << "<=" << mR;
+        if (mI > 0) {
+            if (mI == 1)
+                cout << "+i";
+            else
+                cout << "+" << mI << "i";
+        }
+        else {
+            if (mI == -1)
+                cout << "-i";
+            else
+                cout << mI << "i";
+        }
+    }
+    cout << endl;
+}
+
+CComplex CComplex::Add(const CComplex& zC) const {
+    return CComplex(mR + zC.mR, mI + zC.mI);
+}
+
+CComplex CComplex::Substract(const CComplex& zC) const {
+    return CComplex(mR - zC.mR, mI - zC.mI);
+}
+
+CComplex CComplex::Multiply(const CComplex& zC) const {
+    double realPart = mR * zC.mR - mI * zC.mI;
+    double imagPart = mR * zC.mI + mI * zC.mR;
+    return CComplex(realPart, imagPart);
+}
+
+CComplex CComplex::Divide(const CComplex& zC) const {
+    // 复数除法：(a+bi) / (c+di) = [(ac+bd) + (bc-ad)i] / (c²+d²)
+    double denominator = zC.mR * zC.mR + zC.mI * zC.mI;
+
+    if (denominator == 0) {
+        throw runtime_error("Division by zero: denominator cannot be zero");
+    }
+
+    double realPart = (mR * zC.mR + mI * zC.mI) / denominator;
+    double imagPart = (mI * zC.mR - mR * zC.mI) / denominator;
+
+    return CComplex(realPart, imagPart);
+}
+
+double CComplex::abs() const {
+	return sqrt(mR * mR + mI * mI);
+}
---
+++ 通用比大小/main.cpp
@@ -0,0 +1,32 @@
+﻿#include "Compare.h"
+#include <iostream>
+#include <string>
+#include "CComplex.h"
+using namespace std;
+int main() {
+	// 整型比较
+	int intA = 10;
+	int intB = 20;
+	cout << Compare<int>::IsBigger(intA, intB) << endl;
+	cout << Compare<int>::IsSmaller(intA, intB) << endl;
+	cout << Compare<int>::IsEqual(intA, intB) << endl;
+	// 浮点型比较
+	double doubleA = 10.5;
+	double doubleB = 20.5;
+	cout<<Compare<double>::IsBigger(doubleA, doubleB)<<endl;
+	cout << Compare<double>::IsSmaller(doubleA, doubleB) << endl;
+	cout << Compare<double>::IsEqual(doubleA, doubleB) << endl;
+	// 字符串比较
+	string strA = "apple";
+	string strB = "banana";
+	cout<<Compare<string>::IsBigger(strA, strB)<<endl;
+	cout << Compare<string>::IsSmaller(strA, strB) << endl;
+	cout << Compare<string>::IsEqual(strA, strB) << endl;
+	// 复数比较
+	CComplex complexA(3.0, 4.0); // 复数3 + 4i
+	CComplex complexB(1.0, 7.0); // 复数1 + 7i
+	cout<<Compare<CComplex>::IsBigger(complexA, complexB)<<endl;
+	cout << Compare<CComplex>::IsSmaller(complexA, complexB) << endl;
+	cout << Compare<CComplex>::IsEqual(complexA, complexB) << endl;
+	return 0;
+}
